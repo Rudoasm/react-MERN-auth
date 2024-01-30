57b8977b159804c88e24f799432c19a39d44b915
Aiman Salam <108806913+Rudoasm@users.noreply.github.com>
2024-01-30 16:16:06 +0530

    connected to db

--- API/index.js
+++ API/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("Listning to server on port 3000...");
