aa81b1268129887a7d3444c22a83e51ab02d0290
Aiman Salam <108806913+Rudoasm@users.noreply.github.com>
2024-01-30 16:46:50 +0530

    added model

---
+++ API/models/user.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    // rules for the model
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    
+  },
+  { timestamps: true }
+//   mongodb adds two other info for each user
+);
+
+const User = mongoose.model('User', userSchema);
+// User is the name of the modul and must be capital
+
+export default User;
