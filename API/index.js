b216ac2b37a8c17bee014e3990c164e6f0e3f384
Aiman Salam <108806913+Rudoasm@users.noreply.github.com>
2024-01-30 17:42:10 +0530

    test api route created

--- API/index.js
+++ API/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/userRoutes.js";
+// add the extension a end cuz this is backend
 
 dotenv.config();
 
@@ -16,3 +18,8 @@ const app = express();
 app.listen(3000, () => {
   console.log("Listning to server on port 3000...");
 });
+
+// apis
+app.use("/API/user", userRoute);
+// the address to make the ai work
+// remember backend has the port of 3000. and this api route is in home page
