9802d4d6011ef5350ac69762d4bf5dec5e047c18
Aiman Salam <108806913+Rudoasm@users.noreply.github.com>
2024-02-02 11:08:23 +0530

    finished the signUp part

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,6 +6,8 @@ export default function SignUp() {
 
   const [formdata, setformdata] = useState({});
   // initial value, an empty object
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setformdata({ ...formdata, [e.target.id]: e.target.value });
@@ -17,19 +19,34 @@ export default function SignUp() {
     // should be asynchronous, cuz it should wait until data is passed. if fetch is used await is used so async is mandatory
     e.preventDefault();
     // prevents refreasing the page upon clicking submit button
-    const res = await fetch("/API/auth/signUp", {
-      // esssentail if fetch is used instead of axios 
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formdata),
-    });
-
-    const data = await res.json();
-    console.log(data);
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/API/auth/signUp", {
+        // esssentail if fetch is used instead of axios 
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formdata),
+      });
+  
+      const data = await res.json();
+      console.log(data);
+    
+  
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
   };
-
+    
   return (
     <div>
       <h1>Sign up page</h1>
@@ -65,12 +82,13 @@ export default function SignUp() {
           />
         </label>
         <br />
-        <button type="submit">Sign Up</button>
+        <button type="submit" disabled={loading}> {loading ? 'Loading...' : 'Sign Up'}</button>
       </form>
 
       <p>
         Already have an account? <Link to="/signIn">Sign In</Link>
       </p>
+      <p className="error-msg">{error && 'Something went wrong!'}</p>
     </div>
   );
 }
